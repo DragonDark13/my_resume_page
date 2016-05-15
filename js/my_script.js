ba923e8a490ca93d9cb88e7aa104cb017541dc17
DragonDark13 <alekz.zak@mail.ru>
2016-05-15 07:59:27 +0300

    fly_shutter_and_imprint

--- js/my_script.js
+++ js/my_script.js
@@ -1,45 +1,18 @@
 jQuery(function(){
 
-    /* classToAdd - Класс добавляемый элементу когда тот появляется в области просмотра. Список классов через пробел, берется в кавычки.
-classToRemove - Класс, удаляемый перед применением "classToAdd" к элементу. Список классов через пробел, берется в кавычки.
-
-offset - Задержка появления элементов, относительно просматриваемой области. Указывается числом (значение в пикселях). Можно указывать процентное соотношение (указывать в кавычках, со знаком "%")
-
-invertBottomOffset - Делает значение смещения отрицательным значением. Указывается логическим значением ("true" или "false").
-
-repeat - Удаляет добавляемый класс когда элемент вне области просмотра, делая фукцию повторяемой. Указывается логическим значением ("true" или "false").
-
-callbackFunction - Функция запускаемая после добавления класса. Возвращает "add" или "remove", в зависимости от того был ли класс добавлен или удален
-
-scrollHorizontal - Установить "true" если сайт имеет горизонтальный скролл, вместо вертикального. Указывается логическим значением ("true" или "false").
- */
-
-
-// jQuery('.my_skills .thumbnail').addClass('invisible');
-
-
-var CheckerOffset = 180;
+var CheckerOffset = 0;
 
 jQuery('.track .fa').viewportChecker({
-classToAdd: 'FlyInLeft',
-      classToRemove: 'invisible',
-      offset: 140,
-      invertBottomOffset: false,
-      repeat: false,
+      offset: CheckerOffset,
       callbackFunction: function(elem, action){
-
-elem.addClass('active ');
-
+        elem.addClass('active ');
       },
 });
 
     jQuery('.my_skills .thumbnail').viewportChecker({
-      classToAdd: ' animated zoomIn',
+      classToAdd: 'animated zoomIn',
       classToRemove: 'invisible',
-      offset: CheckerOffset,
-      invertBottomOffset: false,
-      repeat: false,
-      callbackFunction: function(elem, action){},
+      offset: CheckerOffset
   });
 
 
